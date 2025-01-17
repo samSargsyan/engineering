206bff3aa5e081d3001a8a570e18d8d833ef190f
samSargsyan <samuelsargsyan2010@gmail.com>
2025-01-16 19:47:55 -0800

    ad route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/engineering" element={<Home />} />
         </Routes>
       </Router>
     </div>
